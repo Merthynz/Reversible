ecc6d62f3e9f7787450eeb67536bafdc9ff3f985
Merthynz <rerive@gmail.com>
2022-09-22 19:11:35 -0400

    added logic to app page and contents to code page  Changes to be committed: 	new file:   .vscode/launch.json 	modified:   app.html 	modified:   code.html 	modified:   css/site.css 	deleted:    img/android-chrome-192x192.png 	deleted:    img/android-chrome-512x512.png 	deleted:    img/apple-touch-icon.png 	deleted:    img/favicon-16x16.png 	deleted:    img/favicon-32x32.png 	deleted:    img/favicon.ico 	deleted:    img/favicon_io.zip 	new file:   img/reversible_logo.png 	deleted:    img/ziento_logo.png 	modified:   index.html 	modified:   js/site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get the string from the page
+// Controller function
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Revese the string
+// Logic function 
+function reverseString(userString){
+
+    let revString = [];
+    
+    // Reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+}
+
+// Display our reversed string to the user
+// View function
+function displayString(revstring){
+
+    // Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
